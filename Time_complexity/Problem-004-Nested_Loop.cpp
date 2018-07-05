8b73112cf51539b06b9777afe8b01e8b4fa6a649
user140112 <shakilsorkar14@gmail.com>
2018-07-05 23:00:22 +0600

    This is simple Nested loop program

---
+++ Time_complexity/Problem-004-Nested_Loop.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int cnt=0;
+
+    for( int i=0; i<n; i++){
+        for( int j=0; j<n; j++){
+            cnt = cnt + 1;
+        }
+    }
+
+    printf("%d\n",cnt);
+
+    return 0;
+}

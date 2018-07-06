11b64d2ae238a5b017b36e955197116f18d781c9
user140112 <shakilsorkar14@gmail.com>
2018-07-06 15:26:45 +0600

    Space complexity analysis of simple program

---
+++ Space_complexity/Problem-001-Simple_Print_Program.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    if( (n%2) == 0 )
+        printf("This is an even number\n");
+    else
+        printf("This is an odd number\n");
+
+    return 0;
+}
